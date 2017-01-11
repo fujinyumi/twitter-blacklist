bd459033c3d85a1572ee56a1d0cda438ded46afd
Miranda Ku <miranda.ku.paly@gmail.com>
2017-01-11 13:06:20 -0800

    added minor performance changes, removed console clogging debug msgs

--- src/background.js
+++ src/background.js
@@ -1,8 +1,7 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		var response = {};
-		
-		console.log("yo got message");
+
 		if (request == 'retrieveStorage') {
 			response.data = localStorage.settings;
 		}
--- src/insert.js
+++ src/insert.js
@@ -1,3 +1,12 @@
+// FLAGS
+var cur_len = 0;
+var next_len = 0;
+var blacklist_changed = false;
+
+var new_settings = {
+	'blacklist': []
+};
+
 var settings = {
 	'blacklist': []
 };
@@ -11,12 +20,17 @@ function apply(event) {
 	if(!event)
 		alert("Twitter Blacklist: Unexpected error at func apply() in insert.js. Please alert the programmer.");
 	retrieveLists(event.data);
+	
 	filterTweets();
 }
 
 function retrieveLists(saveddata) {
 	try {
-		settings = JSON.parse(saveddata);
+		new_settings = JSON.parse(saveddata);
+		if (new_settings.blacklist != settings.blacklist)
+			blacklist_changed = true;
+		else blacklist_changed = false;
+		settings = new_settings;
 	} catch(e) {
 			alert("Your saved settings have been corrupted. Twitter Blacklist has wiped saved data and restarted.");
 	}
@@ -24,22 +38,24 @@ function retrieveLists(saveddata) {
 
 function filterTweets() {
 	var tweets = document.getElementsByClassName("js-tweet-text-container");
-	//console.log(tweets);
-	console.log(tweets.length);
+	
+	cur_length = tweets.length;
+	
 	for (var i = 0; i < tweets.length; i++) {
 		var tweet = tweets[i];
-		console.log(tweet.textContent)
 		for (var j = 0; j < settings.blacklist.length; j++) {
 			var word = settings.blacklist[j];
-			console.log("Word is " + word);
 			if (tweet.textContent.indexOf(word) != -1) {
-				console.log("Found a match");
 				tweet.textContent = "This tweet contains blacklisted content";
 			}
 		}
 	}
 }
 
+init();
 window.setInterval(function () {
-	init();
-}, 1);
+	var tweets = document.getElementsByClassName("js-tweet-text-container");
+	next_len = tweets.length;
+	if (next_len != cur_len || blacklist_changed == true)
+		init();
+}, 5);
